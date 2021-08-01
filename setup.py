750ab3c57418cd71b8851192fa9cbcb2970c17e4
Shawan Mandal <shawan.sm049@gmail.com>
2021-08-01 17:00:54 +0530

    Updated classifier for Python3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ classifiers = [
         "Intended Audience :: Education",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python 3"
+        "Programming Language :: Python :: 3"
     ]
 
 with open("README.md", "r") as readme:
